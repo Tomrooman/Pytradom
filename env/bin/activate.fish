1343602b8fd8fff92e6b0ee0bb7a4242f7375b0a
Tom Rooman <tomrooman@hotmail.com>
2023-06-18 15:35:28 +0200

    feat: default empty venv environment

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/tom/python-trading-bot/env"
+set -gx VIRTUAL_ENV "/home/tom/Pytradom/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
