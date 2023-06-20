11f4ddc170762ab7eca2f2d192239acfe0cd7d94
Tom Rooman <tomrooman@hotmail.com>
2023-06-20 23:54:32 +0200

    feat: basic starting process

--- src/main.py
+++ src/main.py
@@ -1,23 +1,53 @@
-# pyright: reportMissingImports=false
-
 import json
 import os
-import MetaTrader5 as mt5  # type: ignore
+import MetaTrader5 as mt5
+# import time
 
 
-def get_settings():
-    if (os.path.exists(os.path.join(os.path.dirname(__file__), 'bot.settings.json')) == False):
-        raise ImportError('No bot.settings.json file found')
+def get_settings(file_name: str):
+    if (os.path.exists(os.path.join(os.path.dirname(__file__), file_name)) == False):
+        raise ImportError('No {file_name} file found')
 
-    with open(os.path.join(os.path.dirname(__file__), 'bot.settings.json')) as f:
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
         settings = json.load(f)
         return settings
 
 
+def initialize_mt5(settings):
+    if not mt5.initialize(
+        path=settings["mt5"]["path"],
+            login=settings["mt5"]["login"],
+            password=settings["mt5"]["password"],
+            server=settings["mt5"]["server"],
+            timeout=settings["mt5"]["timeout"],
+            portable=settings["mt5"]["portable"]):
+        print("initialize() failed, probably bad credentials, error code =",
+              mt5.last_error())
+        mt5.shutdown()
+        quit()
+
+
+def connect_to_mt5(settings):
+    authorized = mt5.login(
+        login=settings["mt5"]["login"],
+        password=settings["mt5"]["password"],
+        server=settings["mt5"]["server"],
+        timeout=settings["mt5"]["timeout"],
+        portable=settings["mt5"]["portable"]
+    )
+    if not authorized:
+        print("failed to connect to metatrader account, error code =",
+              mt5.last_error())
+        mt5.shutdown()
+        quit()
+
+
 def main():
-    settings = get_settings()
+    settings = get_settings('bot.settings.json')
     print("Main function")
-    print(settings["bot"]["name"])
+    initialize_mt5(settings)
+    connect_to_mt5(settings)
+    print("End of main function")
 
 
 if __name__ == "__main__":
